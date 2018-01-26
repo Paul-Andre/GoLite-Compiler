2fb5db57149ac99df8f3826113efafdb9a0f6031
Paul-Andre HENEGAR <paul-andre.henegar@mail.mcgill.ca>
2018-01-26 13:16:07 -0500

    Now something that should work

---
+++ src/main.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "lex.yy.c" // eh?
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "Requires option\n");
+    return 1;
+  }
+
+  if (strcmp(argv[1], "scan") == 0) {
+    while(yylex()) {}
+    printf("OK\n");
+  }
+  else if (strcmp(argv[1], "tokens") == 0) {
+    while(1) {
+      int token = yylex();
+      if (token == 0) break;
+
+      switch(token) {
+        case tINTVAL:
+        case tFLOATVAL:
+        case tSTRING:
+        case tIDENT:
+          printf("%s %s\n", yytname[yytranslate[token]], yylval.text);
+          break;
+        default:
+          printf("%s\n", yytname[yytranslate[token]]);
+          break;
+      }
+    }
+  }
+  else if (strcmp(argv[1], "parse") == 0) {
+    yyparse();
+    printf("OK\n");
+  }
+  else {
+    fprintf(stderr, "Incorrect option `%s`\n", argv[1]);
+    return 1;
+  }
+
+	return 0;
+}
