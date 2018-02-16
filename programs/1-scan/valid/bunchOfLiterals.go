003baf9b228d3a50a125ec742648727fbfcb6af0
Paul-Andre HENEGAR <paul-andre.henegar@mail.mcgill.ca>
2018-02-15 19:39:30 -0500

    tests scanner for a bunch of literals

---
+++ programs/1-scan/valid/bunchOfLiterals.go
@@ -0,0 +1,39 @@
+// Some integer literals
+// Decimal
+0
+1
+213142151449
+// Octal
+00
+0134
+// Hexadecimal
+0x12Fafd
+0X12Fafd
+
+// Some float literals
+0.
+.0
+000.000
+123.123
+0924.21
+00000000.
+
+// Some rune literals
+'a'
+'b'
+'C'
+'\n'
+'\b'
+'\''
+'\\'
+'"'
+
+// Some string literals
+"asdf asdf asdf "
+"asdfas \\ \n \" asd "
+`asdf asdf asdf '\adf " asdfasd`
+`
+sd  asd f  asdf   asd fa d
+sdf asdf asdf asd
+sdf asd f adf a df 
+`
