07fb5fbbafed625ce67b6c0bb1052b4c1b0bd413
Paul-Andre HENEGAR <paul-andre.henegar@mail.mcgill.ca>
2018-02-15 19:46:13 -0500

    Added keywords to valid scanner test

---
+++ programs/1-scan/valid/keywords.go
@@ -0,0 +1,11 @@
+// I copied these keywords from the spec
+// ATTENTION: This file scanning correctly means nothing since the scanner might think they are identifiers
+// We really need to run `tokens` on this and check that it produces no identifiers
+
+break        default      func         interface    select
+case         defer        go           map          struct
+chan         else         goto         package      switch
+const        fallthrough  if           range        type
+continue     for          import       return       var
+
+print println append
