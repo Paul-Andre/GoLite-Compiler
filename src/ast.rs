d2d74b95a00d3c9560d0406252500ad94edc1017
Paul-Andre HENEGAR <paul-andre.henegar@mail.mcgill.ca>
2018-02-22 14:21:05 -0500

    Started the AST structure; done Expression

---
+++ src/ast.rs
@@ -0,0 +1,57 @@
+
+enum Kind{
+    Undefined,
+    Int,
+    Float64,
+    String,
+    Bool,
+    Rune
+}
+
+struct SourceLocation{
+    line_number: u32
+}
+
+enum Expression {
+    Identifier{ name: String },
+    RawLiteral{ value: String },
+    BinOperation{  
+        op: BinOperator,
+        lhs: Box<ExpressionNode>,
+        rhs: Box<ExpressionNode>
+    },
+    UnOperation{
+        op: UnOperator,
+        rhs: Box<ExpressionNode>,
+    },
+    Index{
+        primary: Box<ExpressionNode>,
+        index: Box<ExpressionNode>,
+    },
+    Selector{
+        primary: Box<ExpressionNode>,
+        name: String
+    },
+    FunctionCall{
+        primary: Box<ExpressionNode>,
+        arguments: Vec<ExpressionNode>
+    },
+    Append{
+        lhs: Box<ExpressionNode>,
+        rhs: Box<ExpressionNode>,
+    },
+
+    TypeCast{
+        expr: Box<ExpressionNode>
+    },
+
+}
+
+struct ExpressionNode {
+    location: SourceLocation,
+    kind: Kind,
+    expression: Expression
+}
+
+    
+
