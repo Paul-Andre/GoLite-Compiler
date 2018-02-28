ccaa2b7af1079cf869683147f3558255fc42d9ef
Pozidriv <youri.tam@gmail.com>
2018-02-27 20:06:45 -0500

    Added a test case with most operators in it

---
+++ programs/1-scan+parse/valid/ALL_OPERATORS.go
@@ -0,0 +1,45 @@
+package ting
+
+func f() {
+    var x,y int
+    x = x+y
+    x = x-y
+    x = x*y
+    x = x/y
+    x = x%y
+    x = x&y
+    x = x|y
+    x = x^y
+    x = x<<y
+    x = x>>y
+    x = x&^y
+
+    x += y
+    x -= x +y
+    x *= x +y
+    x /= x +y
+    x %= x +y
+
+    x &= x +y
+    x |= x +y
+    x ^= x +y
+    x <<= x +y
+    x >>= x +y
+    x &^= x +y
+
+    x = x&&y
+    x = x||y
+    x = x<-y
+    x++
+    x--
+
+    x = x==y
+    x = x<y
+    x = x>y
+    x = !y
+    x = x!=y
+    x <= x +y
+    x >= x +y
+    x := x +y
+    x = (x+y)*x
+}
