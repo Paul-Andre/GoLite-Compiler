27a158aa7daed2b5b1dffa8f71f0206a37729663
Paul-Andre HENEGAR <paul-andre.henegar@mail.mcgill.ca>
2018-03-13 14:28:35 -0400

    Added symbol testing script

---
+++ programs/2-typecheck/valid/bunchOfUnderscores.go
@@ -0,0 +1,45 @@
+// This test is mainly to 
+package main
+
+func init() {
+}
+
+func init() {
+}
+
+type _ struct {}
+type _ int
+
+var _ struct {}
+
+func _() {
+}
+func _(a, b int) {
+}
+
+
+func _(a, b int) struct{ a, b int;} {
+  var c struct{a,b int;}
+  return c
+}
+
+func _(a, b int) struct{ a, b int;} {
+  var c struct{a,b int;}
+  return c
+}
+
+func _(_, _ int) {
+  var _, _ int
+  var main, init int
+  {
+    var _, _ int
+    var main, init int
+    {
+      var _, _ int
+      var main, init int
+    }
+  }
+}
+
+func f(init, main int) {
+}
---
+++ programs/symbol/run.sh
@@ -0,0 +1,51 @@
+RESULTS=()
+
+
+echo -e "\033[93m"
+echo "    symbol matches reference compiler"
+echo "    ======================"
+echo -e -n "\033[0m"
+COUNT=0
+COUNT_PASSED=0
+if [[ -x ~cs520/golitec ]] 
+then
+  for PROGRAM in `find programs/2-typecheck -name "*.go" `
+  do
+    ((COUNT++))
+
+    if ( ./run.sh symbol $PROGRAM > tmp1 2>/dev/null &&
+      ~cs520/golitec symbol < $PROGRAM > tmp2 2>/dev/null &&
+      diff tmp1 tmp2) then
+    ((COUNT_PASSED++))
+    else
+      STATUS_COLOUR="31"
+      echo "$PROGRAM " | tr -d '\n'
+      echo -e -n " \033[0;${STATUS_COLOUR}m[fail]\033[0m"
+      echo
+    fi
+
+  done
+  if [ $COUNT -eq $COUNT_PASSED ]
+  then STATUS_COLOUR="42"
+  else
+    STATUS_COLOUR="41"
+  fi
+  echo -e "\e[${STATUS_COLOUR}m# symbol matches reference: ${COUNT_PASSED}/${COUNT}\e[49m"
+  RESULTS+=("\e[${STATUS_COLOUR}m# symbol matches reference: ${COUNT_PASSED}/${COUNT}\e[49m")
+else 
+
+  STATUS_COLOUR="43"
+  echo -e "\e[${STATUS_COLOUR}m# Can't find ~cs520/golitec\e[49m"
+
+fi
+
+
+
+
+
+
+
+echo RESULTS:
+for i in ${!RESULTS[*]}; do
+	echo -e ${RESULTS[$i]}
+done
