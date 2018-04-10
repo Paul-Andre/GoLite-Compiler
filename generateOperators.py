b3579f282d0925801d81b8ae80a8fa33681cfa2e
Paul-Andre HENEGAR <paul-andre.henegar@mail.mcgill.ca>
2018-04-10 18:29:25 -0400

    Some random testing tool

---
+++ generateOperators.py
@@ -0,0 +1,40 @@
+import random
+
+binaryOperators = [
+"+",
+"-",
+"*",
+"/ ",
+"% ",
+"& ",
+"|",
+"^ ",
+"&^",
+"<<",
+">>"
+]
+
+unarayOperators = [
+"+",
+"-",
+"^",
+"",
+"",
+"",
+]
+
+print("package main")
+print("func main() {")
+
+for _ in range(100):
+    print("\t{");
+    print("\t\tprintln(");
+
+    for _ in range(10):
+        print("\t\t\t"+random.choice(unarayOperators)+str(random.randint(0,100000))+" "+random.choice(binaryOperators))
+
+    print("\t\t\t"+random.choice(unarayOperators)+str(random.randint(0,100000)) + ")" )
+
+    print("\t}");
+
+print("}")
