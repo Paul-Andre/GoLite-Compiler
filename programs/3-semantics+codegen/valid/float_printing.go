c543d96ee7153fb5a7f8a33509968cebfcf0de99
Pozidriv <youri.tam@gmail.com>
2018-04-10 11:05:49 -0400

    Added struct init and float printing

---
+++ programs/3-semantics+codegen/valid/float_printing.go
@@ -0,0 +1,18 @@
+//~+Inf
+//~-Inf
+//~NaN
+//~+3.434342e+000
+//~+3.434342e+014
+//~+3.434342e-009
+
+package main
+func main() {
+    var k float64;
+	println(1.0/k)
+	println(-1.0/k)
+	println(0.0/k)
+	println(3.434342)
+	println(343434200000000.0)
+	println(0.000000003434342)
+}
+
---
+++ programs/3-semantics+codegen/valid/initialization_struct.go
@@ -0,0 +1,11 @@
+//~0 0 0 0
+package main
+type blah struct {
+    a,b,c int;
+    ting [4]int;
+}
+
+func main() {
+    var k blah;
+    println(k.a, k.b, k.c, k.ting[0]);
+}
