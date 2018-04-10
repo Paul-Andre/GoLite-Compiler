fab8680c896ca878a0f521f179192d0e0effd38e
Pozidriv <youri.tam@gmail.com>
2018-04-10 10:29:31 -0400

    Added initialization test

---
+++ programs/3-semantics+codegen/valid/initialization.go
@@ -0,0 +1,81 @@
+//~0
+//~0
+//~0
+//~0
+//~false
+//~false
+//~
+//~
+//~+0.000000e+000
+//~+0.000000e+000
+
+package main
+
+func main() {
+    {
+    var i int;
+    println(i)
+    }
+    {
+    var i []int;
+    //println(i)
+    i=i
+    }
+    {
+    var i [5]int;
+    println(i[0])
+    }
+    {
+    var i []rune;
+    //println(i)
+    i=i
+    }
+    {
+    var i [5]rune;
+    println(i[0])
+    }
+    {
+    var i rune;
+    println(i)
+    }
+    {
+    var i []bool;
+    //println(i)
+    i=i
+    }
+    {
+    var i [5]bool;
+    println(i[0])
+    }
+    {
+    var i bool;
+    println(i)
+    }
+    {
+    var i []string;
+    //println(i)
+    i=i
+    }
+    {
+    var i [5]string;
+    println(i[0])
+    }
+    {
+    var i string;
+    println(i)
+    }
+    {
+    var i []float64;
+    //println(i)
+    i=i
+    }
+    {
+    var i [5]float64;
+    println(i[0])
+    }
+    {
+    var i float64;
+    println(i)
+    }
+
+}
